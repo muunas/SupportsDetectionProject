73f5526748b8396c26e777a7d229b66cb23b65cf
muunas <gonzalo113451@gmail.com>
2026-09-08 14:41:26 +0200

    Actualización Final De Ruta
    Introducción de config.json, guarda la ruta al archivo de tickers que desees

--- src/main.py
+++ src/main.py
@@ -15,30 +15,46 @@ from distance import precio_actual
 
 
 
-## Seleccion de la ruta dependiendo de si se ejecuta desde .exe o desde visual
-if hasattr(sys, "_MEIPASS"):
-
-    BASE_DIR = sys._MEIPASS
-    ruta_ticker = os.path.join(BASE_DIR, "data", "ticker.json")
-else:
-
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    ruta_ticker = os.path.join(BASE_DIR, "..", "data", "ticker.json")
-
-
 years = 1
 max_pct = 5
 ventana = 2
+CONFIG_PATH = "config.json"
+
+
+
 
 ##FUNCIONES PRINCIPALES
-def ejecutar_pipeline():
+
+def cargar_ruta_ticker():
+    if os.path.exists(CONFIG_PATH):
+        with open(CONFIG_PATH, "r") as f:
+            data = json.load(f)
+            return data.get("ruta_ticker")
+    return None
+
+def guardar_ruta_ticker(ruta):
+    with open(CONFIG_PATH, "w") as f:
+        json.dump({"ruta_ticker": ruta}, f)
+
+
+def obtener_ruta_ticker():
+    ruta = cargar_ruta_ticker()
+    if ruta and os.path.exists(ruta):
+        return ruta
+    
+    print("No se ha encontrado la ruta del ticker.json.")
+    ruta = input("Introduce la ruta completa del ticker.json: ").strip()
+    guardar_ruta_ticker(ruta)
+    return ruta
+
+
+def ejecutar_pipeline(rticker,data):
     print("\n==============================================")
     print(" INICIANDO SCREENER DE SOPORTES")
     print("==============================================\n")
 
     # Cargar tickers
-    with open(ruta_ticker, "r") as f:
-        data = json.load(f)
+
 
     tickers = data["tickers"]
     total = len(tickers)
@@ -81,9 +97,9 @@ def ejecutar_pipeline():
 
 ## COMANDOS DE USUARIO
 
-def abrir_editor():
+def abrir_editor(rticker):
     print("\nAbriendo ticker.json...")
-    subprocess.Popen(["notepad.exe", ruta_ticker])
+    subprocess.Popen(["notepad.exe", rticker])
     print("Puedes editar los tickers y guardar.\n")
 
 
@@ -101,14 +117,19 @@ def mostrar_menu():
 def main():
     mostrar_menu()
 
+    ruta_ticker = obtener_ruta_ticker()
+    
+    with open(ruta_ticker, "r") as f:
+        data = json.load(f)
+
     while True:
         comando = input(">>> ").strip().lower()
 
         if comando == "start":
-            ejecutar_pipeline()
+            ejecutar_pipeline(ruta_ticker,data)
 
         elif comando == "edit":
-            abrir_editor()
+            abrir_editor(ruta_ticker)
 
         elif comando == "exit":
             print("\nCerrando programa...")
